f29397a55a0ec62a4d9bff857d22a59c8db3bbde
Millicent <millicenthere@gmail.com>
2025-09-27 00:26:58 -0500

    worked on Js actions

---
+++ js/app.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+    let emailState = false;
+    let emailModal = document.getElementsByClassName('email-modal') [0];
+
+    let closeModal = document.getElementsByClassName('email-modal__close-btn') [0];
+
+    let showModal = () => {
+            if (emailState == false) {
+            emailModal.classList.add('email-modal--visible');
+            emailState = true;
+        }
+    }
+    closeModal.addEventListener('click', () => {
+        emailModal.classList.remove('email-modal--visible')
+    });
+
+    document.body.addEventListener('mouseleave', () => {
+        showModal();
+    });
+
+    console.log(document)
+}
+
